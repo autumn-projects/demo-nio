51b4d78e5190672cbf1f4aa91a7765ae69e61c98
liaowang <ray5197@sina.com>
2019-01-31 15:07:13 +0800

    aio server

--- src/main/java/aio/TimeServer.java
+++ src/main/java/aio/TimeServer.java
@@ -3,6 +3,13 @@ package aio;
 public class TimeServer {
 
     public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
 
+            }
+        }
     }
 }
